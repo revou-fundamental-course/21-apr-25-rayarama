b1d2e74c615ce924d6108adca97f9110980f46c2
rayarama <76159148+rayarama@users.noreply.github.com>
2025-04-29 19:33:21 +0700

    Add for attribute to label and initializing javascript files

---
+++ js/script.js
@@ -0,0 +1,3 @@
+const userInput = document.getElementById("user-input");
+const convResult = document.getElementById("conversion-result");
+const formulaExplain = document.getElementById("formula-explanation");
