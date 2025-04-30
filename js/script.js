888f0500173b81579887f96ba99d9e05cd9a7df4
rayarama <76159148+rayarama@users.noreply.github.com>
2025-04-30 16:39:39 +0700

    Finalizing the whole document

--- js/script.js
+++ js/script.js
@@ -4,7 +4,10 @@ const formulaExplain = document.getElementById("formula-explanation");
 const convertBtn = document.getElementById("convert");
 const resetBtn = document.getElementById("reset");
 const reverseBtn = document.getElementById("reverse");
+const convLabel = document.getElementById("conversion-label");
+const resultLabel = document.getElementById("result-label");
 
+// * Function for conversion
 function celciusToFahrenheit() {
   let input = userInput.value;
   if (isNaN(input) || input.trim() === "") {
@@ -27,7 +30,50 @@ function fahrenheitToCelcius() {
   formulaExplain.value = `(${input} - 32) × (5 ÷ 9) = ${celcius.toFixed(2)}`;
 }
 
-convertBtn.addEventListener("click", function (event) {
+function reverseCalc() {
+  if (convLabel.textContent.includes("Celcius")) {
+    convLabel.innerHTML = "Fahrenheit (&deg;F)";
+    resultLabel.innerHTML = "Celcius (&deg;C)";
+    formulaExplain.placeholder =
+      "((Suhu dalam Fahrenheit) - 32) × (5 ÷ 9) = (Suhu dalam Celcius)";
+    convertBtn.removeEventListener("click", handleCelcConv);
+    convertBtn.addEventListener("click", handleFahrConv);
+  } else if (convLabel.textContent.includes("Fahrenheit")) {
+    convLabel.innerHTML = "Celcius (&deg;C)";
+    resultLabel.innerHTML = "Fahrenheit (&deg;F)";
+    formulaExplain.placeholder =
+      "(Suhu dalam Celcius) × (9 ÷ 5) + 32 = (Suhu dalam Fahrenheit)";
+    convertBtn.removeEventListener("click", handleFahrConv);
+    convertBtn.addEventListener("click", handleCelcConv);
+  }
+}
+
+function resetHandler() {
+  userInput.value = "";
+  convResult.value = "";
+  formulaExplain.value = "";
+}
+
+// * Function for eventHandler
+function handleCelcConv(event) {
   event.preventDefault();
   celciusToFahrenheit();
+}
+
+function handleFahrConv(event) {
+  event.preventDefault();
+  fahrenheitToCelcius();
+}
+
+// * Event Listener
+convertBtn.addEventListener("click", handleCelcConv);
+
+resetBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  resetHandler();
+});
+
+reverseBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  reverseCalc();
 });
